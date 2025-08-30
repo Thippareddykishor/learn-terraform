975fd98befa578f49fc73002b5a35b74242e0a9f
kishor <kishorebsc12@gmail.com>
2025-08-30 13:35:40 +0530

    added

--- 04-modules/main.tf
+++ 04-modules/main.tf
@@ -1,3 +1,8 @@
-module "name" {
+module "null_resources" {
   source = "./null-res-module"
+}
+
+module "resource-count" {
+  source = "./resource-count"
+  input = module.null_resources.resources
 }

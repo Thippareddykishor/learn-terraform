ad0f4fb9a14d7fdd4fb3b2d0cdae0f5271a77607
kishor <kishorebsc12@gmail.com>
2025-11-23 11:41:47 +0530

    added

--- 00-intro/main.tf
+++ 00-intro/main.tf
@@ -1,5 +1,5 @@
 variable "x" {
-  default = "This test"
+  default = "This test 123"
 }
 
 #print variable an output
@@ -9,4 +9,24 @@ output "x" {
 
 output "x1" {
   value = "Value of x - ${var.x}"
+}
+
+variable "z" {
+  default = {
+    name = "john"
+    age = 20
+  }
+}
+
+#List variables
+variable "y" {
+  default = [1,"abc",0.3,false]
+}
+
+output "y1" {
+  value = var.y[3]
+}
+
+output "z1" {
+  value = "Name ${var.z["name"]},  Age ${var.z.age}"
 }
